dd146a4fe3d75b9fd2699ee3c4e4630322ce9e4c
lashan <lashan@appsquire.com>
2022-04-16 12:52:49 -0600

    save

--- navigation/ChatTabNavigator.js
+++ navigation/ChatTabNavigator.js
@@ -3,13 +3,14 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import ChatScreen from '../screens/ChatScreen';
 import { NavigationContainer } from '@react-navigation/native';
+import CustomNavigationBar from './CustomNavigationBar';
 
 const Stack = createStackNavigator();
 
 export default function ChatTabNavigator() {
   return (<NavigationContainer independent>
       <Stack.Navigator screenOptions={{
-          header: CustomHeaderBar,
+          header: (props) => <CustomNavigationBar {...props} />,
         }}>
         <Stack.Screen name="ChatScreen" component={ChatScreen} />
       </Stack.Navigator>
